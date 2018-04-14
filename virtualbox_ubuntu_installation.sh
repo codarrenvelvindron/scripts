d4b530ed836d6e84910bbc9c50d6956a927ad20e
codarrenvelvindron <codarren@hackers.mu>
2018-04-14 11:58:48 +0400

    added vbox extensions

--- virtualbox_ubuntu_installation.sh
+++ virtualbox_ubuntu_installation.sh
@@ -28,4 +28,5 @@ echo "*****Updating system"
 sudo apt-get update
 echo "*****Installing Virtualbox"
 sudo apt-get install virtualbox
+sudo apt-get install virtualbox-guest-additions-iso
 echo "**Virtualbox installation completed!"
